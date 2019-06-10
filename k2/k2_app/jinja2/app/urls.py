e7578a4676beb281d988ab14d3d7787a8bf19c5e
simon <simon.emmott@yahoo.co.uk>
2019-06-10 22:51:11 +0100

    domains in app/urls.py

--- k2/k2_app/jinja2/app/urls.py
+++ k2/k2_app/jinja2/app/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('domain/', include('k2_domain.urls')),
-    path('app/', include('k2_app.urls')),
-    path('admin/', admin.site.urls),
+{% for domain in app.application_domains.all() %}    path('{{domain.name}}/', include('{{domain.name}}.urls')),
+{% endfor %}    path('admin/', admin.site.urls),
 ]
