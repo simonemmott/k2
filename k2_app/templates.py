41ec23b22cb69acb78c538f7aebaf54d050eaae8
simon <simon.emmott@yahoo.co.uk>
2019-06-16 13:50:36 +0100

    Updated templates.index to handle jinja2 mapping

--- k2_app/templates.py
+++ k2_app/templates.py
@@ -1,16 +1,44 @@
 from k2.settings import BASE_DIR, MAIN_DIR
 import os
 import logging
+from k2.jinja2 import environment
+from jinja2 import PackageLoader
 
 logger = logging.getLogger(__name__)
 
-def index(domain, path):
-    search_path = '/'.join([BASE_DIR, domain, 'jinja2', path])
+jinja2_map = {
+        'k2_domain/domain.name': {
+            'name': '{{domain.name}}'
+        },
+        'k2_domain/domain.name/models/model.py': {
+            'name': '[{% for model in domain.models.all() %}{{model.name}}.py,{% endfor %}]',
+            'keys': '[{% for model in domain.models.all() %}model={{model.id}},{% endfor %}]'
+        }
+    }
+
+
+def index(k2_domain, path, **kw):
+    jinja2_env = environment(loader=PackageLoader(k2_domain, 'jinja2'))
+
+    search_path = '/'.join([BASE_DIR, k2_domain, 'jinja2', path])
     if os.path.isdir(search_path):
         idx={}
         logger.debug('Indexing directory: {path}'.format(path=path))
         for file in os.listdir(search_path):
-            idx[file] = '{path}'.format(domain=domain, path='/'.join([path, file]))
+            f_path = '/'.join([path, file])
+            if jinja2_map.get(f_path):
+                name_template = jinja2_env.from_string(jinja2_map.get(f_path)['name'])
+                name = name_template.render(**kw)
+                if name[0] == '[':
+                    names = name[1:-2].split(',')
+                    keys_template = jinja2_env.from_string(jinja2_map.get(f_path)['keys'])
+                    keys = keys_template.render(**kw)[1:-2].split(',')
+                    for i in range(len(names)):
+                        idx[names[i]] = '{path}&{key}'.format(path=f_path, key=keys[i])
+                else:
+                    idx[name] = '{path}'.format(path=f_path)
+            else:
+                idx[file] = '{path}'.format(path=f_path)
         return idx
     else:
         raise ValueError("The path '{path} is not a directory".format(path=search_path))
--- k2_domain/tests.py
+++ k2_domain/tests.py
@@ -3,9 +3,32 @@ from jinja2 import Environment
 from k2.jinja2 import environment
 from jinja2 import PackageLoader
 import json
+from k2_app import templates
+from posix import lstat
 
 jinja2_env = environment(loader=PackageLoader('k2_domain', 'jinja2'))
 
+def test_domain():
+        class Object(object):
+            pass
+        class List(object):
+            def __init__(self, lst):
+                self.lst = lst
+            def all(self):
+                return self.lst
+                
+        domain = Object()
+        domain.name = 'DOMAIN_NAME'
+        model1 = Object()
+        model1.id = 1
+        model1.name = 'MODEL_1'
+        model2 = Object()
+        model2.id = 2
+        model2.name = 'MODEL_2'
+        domain.models = List([model1, model2])
+        
+        return domain
+    
 # Create your tests here.
 class Jinja2Tests(TestCase):
     
@@ -16,19 +39,27 @@ class Jinja2Tests(TestCase):
         
     def test_list_from_string(self):
         template = jinja2_env.from_string('[{% for model in domain.models %}{{model.name}}.py,{% endfor %}]')
-        class Domain(object):
-            pass
-        class Model(object):
-            pass
-        domain = Domain()
-        domain.name = 'NAME'
-        model1 = Model()
-        model1.name = 'MODEL_1'
-        model2 = Model()
-        model2.name = 'MODEL_2'
-        domain.models = [model1, model2]
         
-        lst = template.render(domain=domain)[1:-2].split(',')
+        lst = template.render(domain=test_domain())[1:-2].split(',')
         self.assertEquals('MODEL_1.py', lst[0])
         self.assertEquals('MODEL_2.py', lst[1])
+        
+class IndexTests(TestCase):
+    
+    def test_indexes(self):
+
+        index = templates.index('k2_domain', 'k2_domain', domain=test_domain())
+        self.assertEquals(1, len(index))
+        self.assertTrue('DOMAIN_NAME' in index.keys())
+        self.assertEquals('k2_domain/domain.name', index.get('DOMAIN_NAME'))
+        
+        index = templates.index('k2_domain', 'k2_domain/domain.name/models', domain=test_domain())
+        self.assertEquals(3, len(index))
+        self.assertTrue('__init__.py' in index.keys())
+        self.assertEquals('k2_domain/domain.name/models/__init__.py', index.get('__init__.py'))
+        self.assertTrue('MODEL_1.py' in index.keys())
+        self.assertEquals('k2_domain/domain.name/models/model.py&model=1', index.get('MODEL_1.py'))
+        self.assertTrue('MODEL_2.py' in index.keys())
+        self.assertEquals('k2_domain/domain.name/models/model.py&model=2', index.get('MODEL_2.py'))
+
         
