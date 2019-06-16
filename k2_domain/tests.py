10d1979a66d38754a637a3f3d10f70331e6991a4
simon <simon.emmott@yahoo.co.uk>
2019-06-16 11:31:45 +0100

    created k2_domain templates

--- k2_domain/tests.py
+++ k2_domain/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from jinja2 import Environment
 
 # Create your tests here.
+class Jinja2Tests(TestCase):
+    
+    def test_template_from_string(self):
+        template = Environment.from_string('Hello {{target}}!')
+        output = template.render(target='World')
+        self.assertEqual('Hello World!', output)
+        
+#    def test_list_from_string(self):
+#        template = Environment.from_string('[{% for model in domin.models %}{{model.name}}.py,{% endfor %}]')
+#        model1 = {}
+#        domain = {}
+#        setattr(model1, 'name', 'model1')
+#        model2 = {}
+#        setattr(model2, 'name', 'model2')
+#        setattr(domain, 'models', [model1, model2])
+#        output = template.render(domain=domain)
+        print(output)
